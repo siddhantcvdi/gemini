d6e2397da3c976ed138d715f611e390879782b4e
Siddhant <siddhantchaturvedi2004@gmail.com>
2024-12-13 21:20:27 +0530

    Fixed burger menu issue

--- src/components/BurgerMenu.jsx
+++ src/components/BurgerMenu.jsx
@@ -5,7 +5,7 @@ function BurgerMenu() {
   const {isExpanded, setIsExpanded} = useContext(GlobalContext)
   return (
     <div
-      className="w-12 h-12 flex items-center justify-center hover:bg-[#353739] rounded-full cursor-pointer ml-3 mt-3 absolute"
+      className="w-12 h-12 flex items-center justify-center hover:bg-[#353739] rounded-full cursor-pointer ml-3 mt-3 absolute z-10"
       onClick={() => setIsExpanded((isExpanded) => !isExpanded)}
     >
       <img src={burger} alt="" className="w-[18px] h-[18px]" />
