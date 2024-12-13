8da3e08e4c69b1447fbc17c26079871b994a3dc2
Siddhant <siddhantchaturvedi2004@gmail.com>
2024-12-14 02:20:21 +0530

    z index fix

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -8,7 +8,7 @@ function Topbar() {
     <div className="w-[100%] h-14 bg-[#1e1f20] p-6 ">
       <div
         onClick={()=>{setResult('')}}
-        className={`absolute z-20 top-0 mt-3 right-2 w-[125px] flex h-[48px] rounded-full sm:hidden ml-1 items-center justify-between p-3 cursor-pointer} active:bg-[#32373d]`}>
+        className={`absolute z-10 top-0 mt-3 right-2 w-[125px] flex h-[48px] rounded-full sm:hidden ml-1 items-center justify-between p-3 cursor-pointer} active:bg-[#32373d]`}>
         <img src={plus} alt="" />
         <p className={`text-[#e3e3e3] text-[14px] font-bold mr-2 text-nowrap `}>New Chat</p>
       </div>
