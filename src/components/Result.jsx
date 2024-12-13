0b585723db4b2fa7acf4807622a5678f23c2d154
Siddhant <siddhantchaturvedi2004@gmail.com>
2024-12-13 23:30:21 +0530

    Added Welcome text, Changed sidebar width

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -5,7 +5,11 @@ function Result() {
   const {result} = useContext(GlobalContext);
   if(result == ''){
     return(
-      <div className='w-full flex h-[100%]'>
+      <div className='w-full flex h-[100%] justify-center items-center text-3xl font-semibold' style={{
+        backgroundImage: 'linear-gradient(90deg, rgba(77,130,239,1) 7%, rgba(86,43,250,1) 24%, rgba(214,102,117,1) 54%)',
+        WebkitBackgroundClip: 'text',
+        WebkitTextFillColor: 'transparent',
+      }}>
         <div className=''>Hello, This is DedGemini</div>
       </div>
     )
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -21,7 +21,7 @@ function Sidebar() {
         window.addEventListener('resize', handleResize)
     },[])
     return (
-        <nav className={`shadow-2xl ${isExpanded?'w-[320px] p-3':isMobile? 'w-0 p-0 m-0' : 'w-[72px] p-3'} box-border h-screen bg-[#282a2c] duration-300 ease-in-out rounded-r-lg pt-[100px] ${isMobile?'absolute z-10':'static'} `}>
+        <nav className={`shadow-2xl ${isExpanded?'w-[400px] p-3':isMobile? 'w-0 p-0 m-0' : 'w-[72px] p-3'} box-border h-screen bg-[#282a2c] duration-300 ease-in-out rounded-r-lg pt-[100px] ${isMobile?'absolute z-10':'static'} `}>
             <div className={`${isExpanded?'w-[125px]':'w-[40px]'} h-[40px] rounded-full bg-[#202123] ml-1 flex items-center justify-between p-2 duration-300 cursor-pointer ${isMobile && !isExpanded?'hidden':'flex'} active:bg-[#32373d]`}>
                 <img src={plus} alt="" />
                 <p className={`text-[#e3e3e3] text-[14px] font-bold mr-2  ${isExpanded?'block':'hidden'} text-nowrap `}>New Chat</p>
