3b6d1bc1cda4d186e27a1e32ec678716338dbe2b
Siddhant <siddhantchaturvedi2004@gmail.com>
2024-12-13 23:33:26 +0530

    font fix

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -5,7 +5,7 @@ function Result() {
   const {result} = useContext(GlobalContext);
   if(result == ''){
     return(
-      <div className='w-full flex h-[100%] justify-center items-center text-3xl font-semibold' style={{
+      <div className='w-full flex h-[100%] justify-center items-center text-3xl font-normal' style={{
         backgroundImage: 'linear-gradient(90deg, rgba(77,130,239,1) 7%, rgba(86,43,250,1) 24%, rgba(214,102,117,1) 54%)',
         WebkitBackgroundClip: 'text',
         WebkitTextFillColor: 'transparent',
