7c5cda63abef0242f920be89361ef6f72228d867
Siddhant <siddhantchaturvedi2004@gmail.com>
2024-12-13 21:40:46 +0530

    shit

--- src/components/BurgerMenu.jsx
+++ src/components/BurgerMenu.jsx
@@ -2,10 +2,10 @@ import React, { useContext } from "react";
 import burger from '../assets/burger.svg'
 import { GlobalContext } from "../contexts/GlobalContext";
 function BurgerMenu() {
-  const {isExpanded, setIsExpanded} = useContext(GlobalContext)
+  const {isExpanded, setIsExpanded, isMobile} = useContext(GlobalContext)
   return (
     <div
-      className="w-12 h-12 flex items-center justify-center hover:bg-[#353739] rounded-full cursor-pointer ml-3 mt-3 absolute z-20"
+      className={`w-12 h-12 flex items-center justify-center ${isMobile?'bg-[#353739]':'hover:bg-[#353739]'} rounded-full cursor-pointer ml-3 mt-3 absolute z-20`}
       onClick={() => setIsExpanded((isExpanded) => !isExpanded)}
     >
       <img src={burger} alt="" className="w-[18px] h-[18px]" />
