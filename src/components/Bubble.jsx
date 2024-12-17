9fe35bf1bfdf0b437f2bf30b3292af9494cd85b5
siddhantcvdi <siddhantchaturvedi2004@gmail.com>
2024-12-17 23:32:17 +0530

    bubble overflow check

--- src/components/Bubble.jsx
+++ src/components/Bubble.jsx
@@ -4,7 +4,7 @@ import ReactMarkdown from "react-markdown";
 function Bubble({ type, text }) {
   return (
     <div className={`w-full flex ${type === "query"?"justify-start":"justify-end"}`}>
-      <div className={`w-fit max-w-[680px] h-fit rounded-xl ${type === 'query'?' bg-blue-700 p-2':'bg-slate-700 p-4'} overflow-hidden`}>
+      <div className={`w-fit max-w-[680px] h-fit rounded-xl ${type === 'query'?' bg-blue-700 p-2':'bg-slate-700 p-4'} overflow-x-scroll`}>
         <ReactMarkdown>{text}</ReactMarkdown>
       </div>
     </div>
