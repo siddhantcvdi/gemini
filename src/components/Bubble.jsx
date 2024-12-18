35ead13d5ece60ce692525b8c8f48e8704854ecb
siddhantcvdi <siddhantchaturvedi2004@gmail.com>
2024-12-18 21:18:39 +0530

    Bubble position change

--- src/components/Bubble.jsx
+++ src/components/Bubble.jsx
@@ -3,7 +3,7 @@ import ReactMarkdown from "react-markdown";
 
 function Bubble({ type, text }) {
   return (
-    <div className={`w-full flex ${type === "query"?"justify-start":"justify-end"}`}>
+    <div className={`w-full flex ${type === "query"?"justify-end":"justify-start"}`}>
       <div className={`w-fit h-fit rounded-xl ${type === 'query'?' bg-blue-700 p-2':'bg-slate-700 p-4'} overflow-x-scroll`}>
         <ReactMarkdown>{text}</ReactMarkdown>
       </div>
