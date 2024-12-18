d4b2d332114df7e0e4e4c2d0fe484e7bc4bae8e0
siddhantcvdi <siddhantchaturvedi2004@gmail.com>
2024-12-18 19:02:37 +0530

    dd

--- src/components/HChat.jsx
+++ src/components/HChat.jsx
@@ -7,7 +7,7 @@ function HChat({session}) {
     <div className='w-full rounded-full font-semibold text-[#ffffff] flex items-center p-2 text-sm hover:bg-[#333537] pl-4 cursor-pointer'
     onClick={()=>{
       setSession(session)
-      setActiveSessionId(session.sessionId)
+      setActiveSessionId(session?.sessionId)
     }}>
       <img src={chat} alt="" className='mr-4'/>
       <p className='pr-4 overflow-hidden text-ellipsis'>{session?.messages[0]?.sentQuery}</p>
