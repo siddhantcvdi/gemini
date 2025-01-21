1417637e3291ee27da7d07ab9c23412068128e78
siddhantcvdi <siddhantchaturvedi2004@gmail.com>
2025-01-21 12:28:27 +0530

    Theme change

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,10 @@ import { GlobalContext } from "./contexts/GlobalContext";
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100 text-center">
-      <h1 className="text-4xl font-bold text-gray-800">We’re Under Maintenance</h1>
-      <p className="text-lg text-gray-600 mt-4">
-        Our site is currently unavailable. Please check back later.
+    <div className="flex flex-col items-center justify-center h-screen bg-[#1e1f20] text-center">
+      <h1 className="text-4xl font-bold text-white">We’re Under Maintenance</h1>
+      <p className="text-lg text-neutral-400 mt-4">
+        Dedgemini is currently unavailable. Please check back later.
       </p>
     </div>
   );
